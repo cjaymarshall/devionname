ffbe792dc8a452ceb6bb7e169735a892ec48eb4c
C Jay Marshall <cjaymarshall1@hotmail.com>
2017-02-28 05:05:53 -0700

    added public folder and app index.html

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.send('App Root');
+	});
 
 http.listen(PORT, function() {
 	console.log("Server started!");
