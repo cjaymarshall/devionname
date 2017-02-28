897b1eb35efe6b1797e866513e24215e6736743e
C Jay Marshall <cjaymarshall1@hotmail.com>
2017-02-28 05:22:18 -0700

    added testExp get request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var PORT = process.env.PORT||3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var testExp = "This is a test";
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -9,6 +10,11 @@ app.get('/', function (req, res) {
 	res.send('App Root');
 	});
 
+app.get'/test/', function (req, res) {
+	res.send(testExp);
+}
+
 http.listen(PORT, function() {
 	console.log("Server started!");
 });
+
